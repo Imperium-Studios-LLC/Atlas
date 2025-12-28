b8a78867f7cb4eb8f302f82b8d1ce58c30f6436e
William <williamqmckenzie@gmail.com>
2025-12-28 15:06:29 -0400

    Added new files

--- atlas/atlas.go
+++ atlas/atlas.go
@@ -1,98 +1,11 @@
 package atlas
 
 const DENSITY_LOW = 10
-const DENSITY_MED = 50
-const DENSITY_HIGH = 200
-
-type Tile struct {
-	X int `json:"x"`
-	Y int `json:"y"`
-	Value int8 `json:"value"`
-}
-
-func (tile Tile) getX() float64 {
-	return float64(tile.X)
-}
-
-func (tile Tile) getY() float64 {
-	return float64(tile.Y)
-}
-
-func NewTile(x int, y int) Tile {
-	return Tile{
-		X: x,
-		Y: y,
-	}
-}
-
-
-type Cell struct {
-	Origin Point
-	Tiles []Tile `json:"tiles"`
-}
-
-func (cell *Cell) addTile(tile Tile) {
-	cell.Tiles = append(cell.Tiles, tile)
-}
-
-func NewCell(origin Point) Cell {
-	return Cell{
-		Origin: origin,
-		Tiles: []Tile{},
-	}
-}
-
-type World struct {
-	points []Point
-	triangles []Triangle
-	
-	Cells []Cell `json:"cells"`
-	Size int `json:"size"`
-}
-
-func (world *World) PrintPoints() []Point {
-	return world.points
-}
-
-func (world *World) GetNearestCell(tile Vector) *Cell {
-	nearest := &world.Cells[0]
-	for idx := range world.Cells {
-		cell := &world.Cells[idx]
-		if distance(nearest.Origin, tile) > distance(cell.Origin, tile) {
-			nearest = cell
-		}
-	}
-	
-	return nearest
-}
-
-func (world *World) Populate(density int) {
-	world.points = generatePoints(density, world.Size, 11)
-	world.triangles = CreateTriangles(world.points)
-	
-	for _, point := range world.points {
-		world.Cells = append(world.Cells, NewCell(point))
-	}
-	
-	for x := 0; x < world.Size; x++ {
-		for y := 0; y < world.Size; y++ {
-			tile := NewTile(x, y)
-			cell := world.GetNearestCell(tile)
-			cell.addTile(tile)
-		}
-	}
-}
-
-func GenerateData(n int, radius int, seed int64) []Triangle {
-	points := generatePoints(n, radius, seed)
-	triangles := CreateTriangles(points)
-	return triangles
-}
+const DENSITY_MED = 100
+const DENSITY_HIGH = 500
 
 func GenerateWorld(size int) *World {
-	world := new(World)
-	world.Size = size
-	world.Populate(DENSITY_HIGH)
+	world := newWorld(DENSITY_MED, size, 100)
 	return world
 }
 
--- atlas/geometry.go
+++ atlas/geometry.go
@@ -1,159 +1,53 @@
 package atlas
 
-import (
-	"math"
-)
+import "math"
 
-type Vector interface {
-	getX() float64
-	getY() float64
-}
-
-type Point struct {
-	X float64 `json:"x"`
-	Y float64 `json:"y"`
-}
-
-func (point Point) getX() float64 {
-	return point.X
-}
-func (point Point) getY() float64 {
-	return point.Y
-}
-
-func distance(vector1 Vector, vector2 Vector) float64 {
-	return math.Pow(math.Pow(vector1.getX() - vector2.getX(), 2) + math.Pow(vector1.getY() - vector2.getY(), 2), 0.5)
-}
-
-func (point1 Point) add(point2 Point) Point {
-	return Point{
-		X: point1.X + point2.X,
-		Y: point1.Y + point2.Y,
-	}
-}
-
-func (point1 Point) subtract(point2 Point) Point {
-	return Point{
-		X: point1.X - point2.X,
-		Y: point1.Y - point2.Y,
-	}
-}
-
-func (point1 Point) multiply(point2 Point) Point {
-	return Point{
-		X: point1.X * point2.X,
-		Y: point1.Y * point2.Y,
-	}
-}
-
-func (point Point) sqr() Point {
-	return Point{
-		X: math.Pow(point.X, 2),
-		Y: math.Pow(point.Y, 2),
-	}
-}
-
-func (point1 Point) divide(point2 Point) Point {
-	return Point{
-		X: point1.X / point2.X,
-		Y: point1.Y / point2.Y,
-	}
-}
-
-func NewPoint(x float64, y float64) Point {
-	return Point{
-		X: x,
-		Y: y,
-	}
-}
-
-
-
-type Triangle struct {
-	Points [3]Point `json:"points"`
-	center Point `json:"center"`
-	radius float64 `json:"radius"`
-}
-
-func (triangle Triangle) IncludesPoint(point Point) bool {
-	for _, p := range triangle.Points {
-		if p.X == point.X && p.Y == point.Y {
-			return true
-		}
-	}
-	return false
-}
-
-func (triangle Triangle) withinCircumcircle(point Point) bool {
-	return distance(point, triangle.center) <= triangle.radius
-}
-
-func (triangle Triangle) validDelauney(points *[]Point) bool {
-	for _, point := range *points {
-		if triangle.withinCircumcircle(point) && !triangle.IncludesPoint(point) {
-			return false
-		}
-	}
-	return true
-}
-
-func (triangle Triangle) reform(point Point, points *[]Point) []Triangle {
-	validTriangles := []Triangle{}
-	triangles := []Triangle{
-		NewTriangle([3]Point{
-			point,
-			triangle.Points[0],
-			triangle.Points[1],
-		}),
-		NewTriangle([3]Point{
-			point,
-			triangle.Points[1],
-			triangle.Points[2],
-		}),
-		NewTriangle([3]Point{
-			point,
-			triangle.Points[2],
-			triangle.Points[0],
-		}),
-	}
+func addPoint(triangles []Triangle, pt Point, pts *[]Point) []Triangle {
+	newTriangles := []Triangle{}
 	
 	for _, triangle := range triangles {
-		if triangle.validDelauney(points) {
-			validTriangles = append(validTriangles, triangle)
+		if triangle.withinCircumcircle(pt) {
+			newTriangles = append(newTriangles, triangle.reform(pt, pts)...)
+		} else {
+			newTriangles = append(newTriangles, triangle)
 		}
 	}
 	
-	return validTriangles
+	return newTriangles
 }
 
-func circumcenter(points [3]Point) Point {
-	p1 := points[0]
-	p2 := points[1]
-	p3 := points[2]
-	
-	a := -(p2.X - p1.X) / (p2.Y - p1.Y)
-	c := -(p3.X - p2.X) / (p3.Y - p2.Y)
-    b := ((p2.Y + p1.Y) / 2) - (a * (p2.X + p1.X) / 2)
-    d := ((p3.Y + p2.Y) / 2) - (c * (p3.X + p2.X) / 2)
-
-    x := (d - b) / (a - c)
-    y := a * x + b
-
-    if math.IsNaN(x) || math.IsNaN(y) {
-        return circumcenter([3]Point{p2, p3, p1})
-    }
-	return Point{
-		X: x,
-		Y: y,
+func createTriangles(pts []Point) []Triangle {
+	maxY := pts[0].Y
+	maxX := pts[0].X
+	for _, pt := range pts {
+		maxY = math.Max(maxY, pt.Y)
+		maxX = math.Max(maxX, pt.X)
 	}
-}
-
-
-func NewTriangle(points [3]Point) Triangle {
-	triangle := new(Triangle)
-	triangle.Points = points
-	triangle.center = circumcenter(points)
-	triangle.radius = distance(triangle.center, points[0])
 	
-	return *triangle
+	addedPts := []Point{
+		Point{0, 0},
+		Point{maxX + 1, 0},
+		Point{0, maxY + 1},
+		Point{maxX + 1, maxY + 1},
+	}
+	
+	triangles := []Triangle{
+		newTriangle([3]Point{
+			addedPts[0],
+			addedPts[1],
+			addedPts[2],
+		}),
+		newTriangle([3]Point{
+			addedPts[1],
+			addedPts[2],
+			addedPts[3],
+		}),
+	}
+	
+	for _, pt := range pts {
+		triangles = addPoint(triangles, pt, &addedPts)
+		addedPts = append(addedPts, pt)
+	}
+	
+	return triangles
 }
